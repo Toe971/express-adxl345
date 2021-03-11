36a5dd2e884c196a2f70da7062b82da86724ac77
toe971 <liew179@gmail.com>
2021-03-11 18:16:42 +0800

    updated

--- expressSqlite3/sqlite3.js
+++ expressSqlite3/sqlite3.js
@@ -1,10 +1,11 @@
 const sqlite3 = require('sqlite3').verbose(); // not needed since use Sequelize?
 const fs = require('fs');
 const { Sequelize, Model, DataTypes } = require('sequelize');
+// look into changing location to be within current directory of app? currently location is also used in docker-compose
 const location = process.env.SQLITE_DB_LOCATION || '/etc/sqlite3/db.sqlite';
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+
 
 
 // below initialisation may have issue on first time as storage is not done out yet, may need to put after init function
@@ -45,8 +46,36 @@ sequelize
             motionBool: {
                 type: DataTypes.BOOLEAN
             }
+        }, {
+            timestamps: true
         })
         // This creates the table if it doesn't exist (and does nothing if it already exists)
         Accelerometer.sync()
     })
+    .catch(err => {
+        console.log("Unable to connect to database: ", err)
+    })
+
+// bodyparser middleware, do not need to import body-parser anymore, this is inbuilt into express
+app.use(express.json())
+
+/* 
+ * GET /accelerometer get all values from accelerometer along with timestamp
+ * GET /accelerometer/<unix time> get all values from accelerometer along with timestamp, convert from UNIX time
+ * to ISO datetime format in order to check
+ * POST /accelerometer add new accelerometer values at the timestamp provided
+ * PUT /accelerometer update accelerometer values at the timestamp provided
+ * motionBool not implemented
+ */
+app.get("/", (req, res) => {
+    res.json({message: "Use /accelerometer to do your HTTP requests"})
+})
+
+// since we are only using accelerometer just put it into a Router
+const accelRouter = express.Router()
+app.use('/accelerometer', accelRouter)
+
+accelRouter.get('/', (req, res, next) => {
+
+})
 
