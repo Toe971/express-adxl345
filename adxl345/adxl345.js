06d6b6ca4ba9b95925abdb709318eaea5b7fef7b
toe971 <liew179@gmail.com>
2021-03-11 19:55:24 +0800

    test on Pi later

---
+++ adxl345/adxl345.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+const { Accelerometer, Board } = require('johnny-five')
+const Raspi = require('raspi-io').RaspiIO
+const board = new Board({
+    io: new Raspi()
+});
+
+board.on("ready", () => {
+    const accelerometer = new Accelerometer({
+        controller: "ADXL345"
+    })
+
+    accelerometer.on("data", () => {
+        const {acceleration, inclination, orientation, pitch, roll, x, y, z} = accelerometer;
+        objToSend = {
+            xAxis: accelerometer.x,
+            yAxis: accelerometer.y,
+            zAxis: accelerometer.z,
+            motionBool: accelerometer.on("change", () => { return 1 }) ? true : false // if 1 true else null false
+        }
+        console.log(objToSend)
+        
+    })
+})
--- expressSqlite3/sqlite3.js
+++ expressSqlite3/sqlite3.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose(); // not needed since use Sequelize?
 const fs = require('fs');
 const { Sequelize, Model, DataTypes } = require('sequelize');
 // look into changing location to be within current directory of app? currently location is also used in docker-compose
-const location = process.env.SQLITE_DB_LOCATION || '/etc/sqlite3/db.sqlite';
+const location = process.env.SQLITE_DB_LOCATION || __dirname + '/db/database.sqlite'; // __dirname is current directory. As fs.mkdirSync takes the location variable as an absolute path, need to 
 const express = require('express')
 const app = express()
 
@@ -10,10 +10,12 @@ const app = express()
 
 // below initialisation may have issue on first time as storage is not done out yet, may need to put after init function
 const makeDirectory = () => {
-    const dirName = require('path').dirName(location)
+    const dirName = require('path').dirname(location)
     if (!fs.existsSync(dirName)) {
+        console.log("Creating database at ", location)
         fs.mkdirSync(dirName, { recursive: true })
     }
+    console.log(`There is a database created at ${location} already.`)
  
 }
 
@@ -30,10 +32,12 @@ const sequelize = new Sequelize(
 // wrapped in sequelize because can use it for authentication, test next time
 // can use Extending Model next time under sequelize docs
 sequelize
+    .authenticate() // is necessary, even though we never provide
     .then(() => {
         console.log("Connection established.")
         // define new table: Acceleration
-        const Accelerometer = sequelize.define("Accelerometer", {
+        // if use inside sequelize.then(...) cannot use const Accelerometer, otherwise will have error pop out
+        Accelerometer = sequelize.define("Accelerometer", {
             xAxis: {
                 type: DataTypes.FLOAT
             },
@@ -47,10 +51,26 @@ sequelize
                 type: DataTypes.BOOLEAN
             }
         }, {
-            timestamps: true
+            timestamps: true // compare it tot timestampAsId, as for createdAt and updatedAt may need to set to faLse
         })
         // This creates the table if it doesn't exist (and does nothing if it already exists)
         Accelerometer.sync()
+
+        const testValues = {
+            xAxis: 3.3333,
+            yAxis: 4.4444,
+            zAxis: 5.5555,
+            motionBool: true,
+            timestampAsId: new Date()
+        }
+
+        const testWrite = async () => {
+            await Accelerometer.create(testValues)
+        }
+        testWrite()
+        console.log(testWrite)
+
+        
     })
     .catch(err => {
         console.log("Unable to connect to database: ", err)
@@ -75,7 +95,17 @@ app.get("/", (req, res) => {
 const accelRouter = express.Router()
 app.use('/accelerometer', accelRouter)
 
-accelRouter.get('/', (req, res, next) => {
-
+accelRouter.get('/', async (req, res, next) => {
+    const accelerometerValues = await Accelerometer.findAll()
+    console.log(accelerometerValues)
+    res.status(200).json({accelerometerValues})
 })
 
+accelRouter.post('/', async (req, res, next) => {
+    // expects shape of POST request to be the same as the GET
+    // i.e. see testValues above
+})
+
+const listener = app.listen(8080, function() {
+    console.log("Listening on port " + listener.address().port);
+  });
